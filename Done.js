30b05e0412d259f64dd97523a33e17608a74077b
MarcoBatman <Marco@terrida.dk>
2020-12-17 09:40:34 +0100

    Initial commit

---
+++ Done.js
@@ -0,0 +1,28 @@
+var liste = [2,6,1,3,4,8,10,7,9,5];
+
+
+function setup() {
+  createCanvas(500,500);
+}
+
+
+function draw() {
+  clear();
+liste.forEach(drawRect);
+}
+
+function drawRect(item, index){
+rect(20+25*index,300,20,-item*10);
+}
+
+
+function keyPressed(){
+if(key == 'k'|| key == 'K'){
+liste = liste.sort(function(a, b){return a-b;});
+}
+if(key=='s'||key=='S'){
+liste = shuffle(liste);
+}
+
+
+}
